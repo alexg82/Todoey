ab4ca1f70a2e0898838811128bdf610f3aaa8baf
Alex Goldstein <alexg82@gmail.com>
2018-10-27 15:24:16 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alex Goldstein on 10/27/18.
+//  Copyright © 2018 Alex Goldstein. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
